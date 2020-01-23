35629dd305503103c709b92effa95a6c9e5f8a4d
rfrahmandhika <rfrahmandhika@gmail.com>
2020-01-23 15:41:10 +0700

    auto scroll header

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,48 @@ $(document).ready(function () {
         side: 'right'
     });
 });
+
+/*Scroll Delay*/
+
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+          $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+          window.location.hash = hash;
+        });
+      }
+    });
+  });
+
+/*Info Auto Scroll*/
+
+$(document).ready(function(){
+        var speed = 0;
+        var scroll = 0;
+        var container = $('#row');
+        var container_w = container.width();
+        var max_scroll = container[0].scrollWidth - container.outerWidth();
+
+        container.on('mousemove', function (e) {
+            var mouse_x = e.pageX - container.offset().left;
+            var mouseperc = 100 * mouse_x / container_w;
+            speed = mouseperc - 50;
+        }).on('mouseleave', function () {
+            speed = 0;
+        });
+
+        function updatescroll() {
+            if (speed !== 0) {
+                scroll += speed / 5;
+                if (scroll < 0) scroll = 0;
+                if (scroll > max_scroll) scroll = max_scroll;
+                $('#row').scrollLeft(scroll);
+            }
+            window.requestAnimationFrame(updatescroll);
+        }
+        window.requestAnimationFrame(updatescroll);
+    });
